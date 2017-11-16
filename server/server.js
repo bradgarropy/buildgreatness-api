66eaf29f6d915a81587336019111e0edf267897c
Brad Garropy <brad_garropy@dell.com>
2017-11-16 13:10:06 -0600

    project  setup.

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+
+// create application
+const app = express()
+
+
+// load env
+dotenv.config()
+
+
+// app settings
+app.set("json spaces", 4)
+
+
+// all routes
+app.get("*", (request, response) => {
+
+    response.json({
+        first_name: "Brad",
+        last_name: "Garropy"
+    })
+
+})
+
+
+// start application
+app.listen(process.env.PORT, () => {
+
+    console.log("Server listening on port %s.", process.env.PORT)
+
+})
